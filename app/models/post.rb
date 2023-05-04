fb6c4c63b74052ab8fb1195058235a26b52563ea
sajibarafatsiddiqui <siddiquisajib75@gmail.com>
2023-05-05 04:50:00 +0600

    Changed in user model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   has_many :likes
 
   scope :total_post_by_author, ->(author_id) { where('author_id = ?', author_id).count if author_id.present? }
+  
   def update_posts_counter(user_id)
     user_id.update(postscounter: self.class.total_post_by_author(user_id))
   end
