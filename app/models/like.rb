6e9da88033d72e17eda67eeffa40d711e97fb57e
sajibarafatsiddiqui <siddiquisajib75@gmail.com>
2023-05-05 00:55:57 +0600

    Added custom methods for posts, likes, users and comments

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,9 @@
 class Like < ApplicationRecord
-  belongs_to :post 
-  belongs_to :author, :class_name => 'User'
+  belongs_to :post
+  belongs_to :author, class_name: 'User'
+
+  scope :total_likes_by_post, ->(post_id) { where('post_id = ?', post_id).count if post_id.present? }
+  def update_likes_counter(post_id)
+    post_id.update(likescounter: self.class.total_likes_by_post(post_id))
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-  has_many :posts
+  has_many :posts, class_name: 'Post', foreign_key: 'author'
   has_many :comments
   has_many :likes
+
+  def first_three_post
+    posts.limit(1)
+  end
 end
