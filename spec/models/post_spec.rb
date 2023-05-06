576941917ee0c18a1ccb0963cf89a467879fe8fb
sajibarafatsiddiqui <siddiquisajib75@gmail.com>
2023-05-06 09:16:59 +0600

    Added the maximum title test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:title) }
+    it { should validate_length_of(:title).is_at_most(250) }
     it { should validate_numericality_of(:commentscounter).only_integer.is_greater_than_or_equal_to(0) }
     it { should validate_numericality_of(:likescounter).only_integer.is_greater_than_or_equal_to(0) }
   end
