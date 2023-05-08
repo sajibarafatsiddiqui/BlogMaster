85b4ebc3c2473f49d00b3f3d4986cfe0486425e6
sajibarafatsiddiqui <siddiquisajib75@gmail.com>
2023-05-09 04:15:00 +0600

    Added controllers for users and posts

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative "boot"
 
 require "rails/all"
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -10,7 +11,7 @@ module BlogMaster
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+# This list is here as documentation only - it's not used
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
