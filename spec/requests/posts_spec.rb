410001393a4b3f28e2970dd0f1c6e90002da60eb
sajibarafatsiddiqui <siddiquisajib75@gmail.com>
2023-05-10 08:29:42 +0600

    Added unit tests for controllers, routes and views to test controllers'

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,44 @@
 require 'rails_helper'
 
-RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe PostsController, type: :controller do
+  before { get :index, params: { user_id: 1 } }
+  it { should respond_with(200) }
+end
+
+RSpec.describe PostsController, type: :controller do
+  it { should route(:get, 'users/1/posts').to(action: :index, user_id: 1) }
+  it { should route(:get, 'users/1/posts/1').to(action: :show, id: 1, user_id: 1) }
+end
+
+RSpec.describe PostsController, type: :controller do
+  before { get :index, params: { user_id: 1 } }
+  it { should render_template('index') }
+end
+
+RSpec.describe PostsController, type: :controller do
+  describe 'GET /users/:user_id/posts' do
+    it 'returns http success' do
+      get :index, params: { user_id: 1 }
+      expect(response.body).to include('I am users posts')
+    end
+  end
+end
+
+RSpec.describe PostsController, type: :controller do
+  before { get :index, params: { user_id: 1, id: 1 } }
+  it { should respond_with(200) }
+end
+
+RSpec.describe PostsController, type: :controller do
+  before { get :show, params: { user_id: 1, id: 1 } }
+  it { should render_template('show') }
+end
+
+RSpec.describe PostsController, type: :controller do
+  describe 'GET /users/:user_id/posts' do
+    it 'returns http success' do
+      get :show, params: { user_id: 1, id: 1 }
+      expect(response.body).to include('I am users post by id')
+    end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,45 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+
+RSpec.describe UsersController, type: :controller do
+  it { should route(:get, 'users').to(action: :index) }
+  it { should route(:get, 'users/1').to(action: :show, id: 1) }
+end
+
+RSpec.describe UsersController, type: :controller do
+  before { get :index }
+  it { should respond_with(200) }
+end
+
+RSpec.describe UsersController, type: :controller do
+  before { get :index }
+  it { should render_template('index') }
+end
+
+RSpec.describe UsersController, type: :controller do
+  describe 'GET /users' do
+    it 'returns http success' do
+      get :index
+      expect(response.body).to include('I am list of users')
+    end
+  end
+end
+
+RSpec.describe UsersController, type: :controller do
+  before { get :index, params: { id: 1 } }
+  it { should respond_with(200) }
+end
+
+RSpec.describe UsersController, type: :controller do
+  before { get :show, params: { id: 1 } }
+  it { should render_template('show') }
+end
+
+RSpec.describe UsersController, type: :controller do
+  describe 'GET /users/:user_id' do
+    it 'returns http success' do
+      get :show, params: { id: 1 }
+      expect(response.body).to include('I am user with id provided as parameter')
+    end
   end
 end
