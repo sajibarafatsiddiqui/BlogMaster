b8c915280bffd2d6e03565a3b9437addefd7ec07
sajibarafatsiddiqui <siddiquisajib75@gmail.com>
2023-05-25 06:48:36 +0600

    Added devise for users and added confirmable to it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
-  has_many :likes, foreign_key: 'author_id'
+  after_initialize :set_defaults
 
   validates_presence_of :name, presence: true
   validates :postscounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
@@ -9,4 +13,9 @@ class User < ApplicationRecord
   def first_three_post
     posts.limit(3)
   end
+
+  def set_defaults
+    self.postscounter ||= 0
+  end
+
 end
