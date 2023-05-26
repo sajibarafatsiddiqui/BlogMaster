3edca5c24594e4d17f6c58f0927ae5ae727053b6
sajibarafatsiddiqui <siddiquisajib75@gmail.com>
2023-05-27 00:29:43 +0600

    Solved linters issues

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,23 +1,24 @@
 module Api
-    class CommentsController < ApplicationController
-      def index
-        @post = Post.find(params[:post_id])
-        @user = @post.author
-        @comments = @post.comments.all
-        render :json => @comments 
-      end
+  class CommentsController < ApplicationController
+    def index
+      @post = Post.find(params[:post_id])
+      @user = @post.author
+      @comments = @post.comments.all
+      render json: @comments
+    end
 
-      def create
-        @post = Post.find(params[:post_id])
-        @user = @post.author
-        @comment = Comment.new(comment_params.merge(author: current_user, post: @post))
-        @comment.save
-        render json: @comment, status: :created
-      end
-      private
+    def create
+      @post = Post.find(params[:post_id])
+      @user = @post.author
+      @comment = Comment.new(comment_params.merge(author: current_user, post: @post))
+      @comment.save
+      render json: @comment, status: :created
+    end
 
-      def comment_params
-        params.require(:comment).permit(:text)
-      end
+    private
+
+    def comment_params
+      params.require(:comment).permit(:text)
     end
   end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,5 @@
-require "application_responder"
-
 class ApplicationController < ActionController::API
-  self.responder = ApplicationResponder
-  respond_to :html
-
-  include ActionController::Helpers
-
   def current_user
     User.find(6)
   end
-
-  helper_method :current_user
 end
